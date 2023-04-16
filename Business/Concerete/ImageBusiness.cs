6535971afbbe7b6cb5890b24bac430984ae30116
Enes TEKLE <tekleenes@gmail.com>
2023-04-17 00:38:51 +0300

    ImageController: Resim dosyası alındı. Gelen resim iki kat çözünürlüğe çıkartılıp geri gönderildi.

---
+++ Business/Concerete/ImageBusiness.cs
@@ -0,0 +1,20 @@
+﻿using Business.Abstract;
+using SixLabors.ImageSharp.Formats.Jpeg;
+
+namespace Business.Concerete
+{
+    public partial class ImageBusiness : IImage_Service
+    {
+        public MemoryStream ResizeImage(Stream file)
+        {
+            Image image = Image.Load(file);
+            image.Mutate(x => x.Resize(new Size(image.Width * 2, image.Height * 2), true));
+
+            using (var memoryStream = new MemoryStream())
+            {
+                image.Save(memoryStream, new JpegEncoder());
+                return memoryStream;
+            }
+        }
+    }
+}
